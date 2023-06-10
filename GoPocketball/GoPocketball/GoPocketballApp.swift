4cea107d54d4bc44cc486e8aa02da2f469ab7844
Peter <ghdtjrgus333@naver.com>
2023-06-10 12:46:31 +0900

    init

---
+++ GoPocketball/GoPocketball/GoPocketballApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoPocketballApp.swift
+//  GoPocketball
+//
+//  Created by Sh Hong on 2023/06/10.
+//
+
+import SwiftUI
+
+@main
+struct GoPocketballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
